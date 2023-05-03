df839644e8c5b22d10d38f8e427bc677cd49e9a9
gdutra2002 <119914021+gdutra2002@users.noreply.github.com>
2023-05-03 14:15:48 -0700

    lots of progress

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,154 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require('inquirer');
+// const fs = require('fs');
+
+const { writeFile } = require('fs').promises;
+
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
+
+const promptUser = () => {
+      return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of the application repository?',
+    },
+    {
+      type: 'input',
+      name: 'motivation',
+      message: 'What was your motivation?',
+    },
+    {
+      type: 'input',
+      name: 'why',
+      message: 'Why did you build this project?',
+    },
+    {
+      type: 'input',
+      name: 'problem',
+      message: 'What problem does it solve?',
+    },
+    {
+      type: 'input',
+      name: 'learn',
+      message: 'What did you learn?',
+    },
+    {
+      type: 'input',
+      name: 'address',
+      message: 'What is the url of the live webpage?',
+    },
+    {
+      type: 'input',
+      name: 'credits',
+      message: 'Add additional personel, colaboration, or institutions to give credit?',
+    },
+    {
+      type: 'input',
+      name: 'badges',
+      message: 'What badges would you like to display?',
+    },
+    {
+      type: 'checkbox',
+      message: 'What main languages does the app use?',
+      name: 'stack',
+      choices: ['HTML', 'CSS', 'JavaScript', 'MySQL', 
+            'MongoDB','Python' , 'Handelbars' , 'React'],
+    },
+    {
+      type: 'list',
+      message: 'What is your preferred method of communication?',
+      name: 'contact',
+      choices: ['email', 'github', 'Linked-In'],
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address',
+    },
+    {
+      type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL.',
+    },
+  ])
+};
+
+
+const generateMD = ({ name, motivation, why, problem, learn, address, credits, badges, stack, contact, github, email, linkedin }) =>
+
+`# ${name}
+
+## Description
+${motivation}.
+${why}.
+${problem}.
+${learn}.
+
+## Installation
+
+${address}
+
+## Usage
+
+![alt text](assets/screenshot.png)
+
+## Credits and Colaborators
+
+${credits}
+
+## License
+
+See MIT license
+
+## Badges
+
+${badges}
+// ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+
+## Features
+${stack}
+
+## How to Contribute
+Prefered method of contact: ${contact}
+Link to source:
+${github}
+Contact author:
+${email}
+Profile
+${linkedin}`;
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
+
+// .then((data) => {
+//       const filename = `${data.name.toLowerCase().split('').join('')}.md`;
+  
+//       fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+//         err ? console.log(err) : console.log('Success!')
+//       );
+//     });
+  
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {
+      const init =() => {
+
+      promptUser()
+      // Use writeFile method imported from fs.promises to use promises instead of
+      // a callback function
+      .then((answers) => writeFile('readme.md', generateMD(answers)))
+      .then(() => console.log('Successfully wrote to readme.md'))
+      .catch((err) => console.error(err));
+};
 
 // Function call to initialize app
 init();
---
+++ Develop/readme.md
@@ -0,0 +1,39 @@
+# gutcrash!
+
+## Description
+funnnn.
+play.
+time.
+mucho.
+
+## Installation
+
+123.com
+
+## Usage
+
+![alt text](assets/screenshot.png)
+
+## Credits and Colaborators
+
+all
+
+## License
+
+See MIT license
+
+## Badges
+
+// ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+
+## Features
+HTML,MySQL,Python,Handelbars
+
+## How to Contribute
+Prefered method of contact: email
+Link to source:
+gomer.hub
+Contact author:
+E@pyle.com
+Profile
+333@lin.com
